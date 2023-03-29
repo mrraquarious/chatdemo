010678a6d911094071fbd14bff28d6eb54f571e8
mrraquarious <40706790+mrraquarious@users.noreply.github.com>
2023-03-30 00:12:38 +0800

    Add files via upload

---
+++ server/server.js
@@ -0,0 +1,49 @@
+import express from 'express'
+import * as dotenv from 'dotenv'
+import cors from 'cors'
+import { Configuration, OpenAIApi } from 'openai'
+
+dotenv.config()
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+app.get('/', async (req, res) => {
+  res.status(200).send({
+    message: 'Hello from openAI!'
+  })
+})
+
+app.post('/', async (req, res) => {
+  try {
+    const prompt = req.body.prompt;
+    const instructions = ``;
+
+    const response = await openai.createChatCompletion({
+        model: "gpt-3.5-turbo",
+        temperature: 0.888,
+        max_tokens: 2048,
+        frequency_penalty: 0,
+        presence_penalty: 0,
+        top_p: 1,
+        messages: [{role: "system", content: instructions}, {role: "user", content: `${prompt}`}], // {role: "assistant", content: ''}
+    });
+    console.log()
+    res.status(200).send({
+      bot: response.data.choices[0].text
+    });
+
+  } catch (error) {
+    console.error(error)
+    res.status(500).send(error || 'Something went wrong');
+  }
+})
+
+app.listen(5000, () => console.log('AI server started on http://localhost:5000'))
